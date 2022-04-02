547313de0b9c57bee30a46e300b2fe1aae05f301
Joe Heck <heckj@mac.com>
2022-04-02 09:33:03 -0700

    reset continuous scales to modifier pattern

--- Sources/SwiftVizScale/LinearScale.swift
+++ Sources/SwiftVizScale/LinearScale.swift
@@ -10,16 +10,16 @@ public struct LinearScale<InputType: ConvertibleWithDouble & NiceValue, OutputTy
     /// The distance or length between the upper and lower bounds of the input domain.
     public let domainExtent: InputType
 
+    /// The lower bound of the input domain.
+    public let rangeLower: OutputType?
+    /// The upper bound of the input domain.
+    public let rangeHigher: OutputType?
+
     /// A transformation value that indicates whether the output vales are constrained to the min and max of the output range.
     ///
     /// If `true`, values processed by the scale are constrained to the output range, and values processed backwards through the scale
     /// are constrained to the input domain.
     public var transformType: DomainDataTransform
-
-    /// A Boolean value that indicates the scale was configured without an explicit domain.
-    ///
-    /// Use ``withDomain(lower:higher:)`` to create a new scale with an explicit domain while keeping the same ``transformType``.
-    public var defaultDomain: Bool
 
     /// The number of ticks desired when creating the scale.
     ///
@@ -32,26 +32,23 @@ public struct LinearScale<InputType: ConvertibleWithDouble & NiceValue, OutputTy
     ///   - higher: The upper bound for the scale's domain.
     ///   - transform: The transform constraint to apply when values fall outside the domain of the scale.
     ///   - desiredTicks: The desired number of ticks when visually representing the scale.
-    public init(from lower: InputType, to higher: InputType, transform: DomainDataTransform = .none, desiredTicks: Int = 10) {
-        precondition(lower < higher)
+    public init(from lower: InputType = 0, to higher: InputType = 1, transform: DomainDataTransform = .none, desiredTicks: Int = 10, rangeLower: OutputType? = nil, rangeHigher: OutputType? = nil) {
+        precondition(lower < higher, "attempting to set an inverted or empty domain: \(lower) to \(higher)")
         transformType = transform
         domainLower = lower
         domainHigher = higher
         domainExtent = higher - lower
         self.desiredTicks = desiredTicks
-        defaultDomain = false
+        if let rangeLower = rangeLower, let rangeHigher = rangeHigher {
+            precondition(lower < higher, "attempting to set an inverted or empty range: \(rangeLower) to \(rangeHigher)")
+        }
+        self.rangeLower = rangeLower
+        self.rangeHigher = rangeHigher
     }
 
-    /// Creates a new linear scale with a default domain.
-    ///
-    /// The default domain for the scale is `0.0...1.0`.
-    /// Use this method to create a placeholder scale that you can refine to a fully configured scale with an updated domain using ``withDomain(lower:higher:)``.
-    /// - Parameters:
-    ///   - transform: The transform constraint to apply when values fall outside the domain of the scale.
-    ///   - desiredTicks: The desired number of ticks when visually representing the scale.
-    public init(exponent _: Double = 1, transform: DomainDataTransform = .none, desiredTicks: Int = 10) {
-        self.init(from: 0, to: 1, transform: transform, desiredTicks: desiredTicks)
-        defaultDomain = true
+    // testing function
+    internal func fullyConfigured() -> Bool {
+        rangeLower != nil && rangeHigher != nil
     }
 
     /// Creates a new linear scale for the upper and lower bounds of the domain range you provide.
@@ -59,8 +56,8 @@ public struct LinearScale<InputType: ConvertibleWithDouble & NiceValue, OutputTy
     ///   - range: A range that represents the scale's domain.
     ///   - transform: The transform constraint to apply when values fall outside the domain of the scale.
     ///   - desiredTicks: The desired number of ticks when visually representing the scale.
-    public init(_ range: ClosedRange<InputType>, transform: DomainDataTransform = .none, desiredTicks: Int = 10) {
-        self.init(from: range.lowerBound, to: range.upperBound, transform: transform, desiredTicks: desiredTicks)
+    public init(_ range: ClosedRange<InputType>, transform: DomainDataTransform = .none, desiredTicks: Int = 10, rangeLower: OutputType? = nil, rangeHigher: OutputType? = nil) {
+        self.init(from: range.lowerBound, to: range.upperBound, transform: transform, desiredTicks: desiredTicks, rangeLower: rangeLower, rangeHigher: rangeHigher)
     }
 
     /// Creates a new power scale for the domain of `0` to the value you provide.
@@ -71,21 +68,71 @@ public struct LinearScale<InputType: ConvertibleWithDouble & NiceValue, OutputTy
     ///   - single: The upper, or lower, bound for the domain.
     ///   - transform: The transform constraint to apply when values fall outside the domain of the scale.
     ///   - desiredTicks: The desired number of ticks when visually representing the scale.
-    public init(_ single: InputType, transform: DomainDataTransform = .none, desiredTicks: Int = 10) {
+    public init(_ single: InputType, transform: DomainDataTransform = .none, desiredTicks: Int = 10, rangeLower: OutputType? = nil, rangeHigher: OutputType? = nil) {
         if single > 0 {
-            self.init(from: 0, to: single, transform: transform, desiredTicks: desiredTicks)
+            self.init(from: 0, to: single, transform: transform, desiredTicks: desiredTicks, rangeLower: rangeLower, rangeHigher: rangeHigher)
         } else {
-            self.init(from: single, to: 0, transform: transform, desiredTicks: desiredTicks)
+            self.init(from: single, to: 0, transform: transform, desiredTicks: desiredTicks, rangeLower: rangeLower, rangeHigher: rangeHigher)
         }
     }
+
+    // MARK: - modifier functions
 
     /// Returns a new scale with the domain set to the values you provide.
     /// - Parameters:
     ///   - lower: The lower bound for the scale's domain.
     ///   - higher: The upper bound for the scale's domain.
-    /// - Returns: A replica of the scale, preserving ``transformType`` while applying new domain values.
-    public func withDomain(lower: InputType, higher: InputType) -> LinearScale<InputType, OutputType> {
-        type(of: self).init(from: lower, to: higher, transform: transformType, desiredTicks: desiredTicks)
+    /// - Returns: A copy of the scale with the domain values you provide.
+    public func domain(lower: InputType, higher: InputType) -> Self {
+        type(of: self).init(from: lower, to: higher, transform: transformType, desiredTicks: desiredTicks, rangeLower: rangeLower, rangeHigher: rangeHigher)
+    }
+
+    /// Returns a new scale with the domain set to the values you provide.
+    /// - Parameters:
+    ///   - range: The range to apply as the scale's domain
+    /// - Returns: A copy of the scale with the domain values you provide.
+    public func domain(_ range: ClosedRange<InputType>) -> Self {
+        type(of: self).init(from: range.lowerBound, to: range.upperBound, transform: transformType, desiredTicks: desiredTicks, rangeLower: rangeLower, rangeHigher: rangeHigher)
+    }
+
+    /// Returns a new scale with the range set to the values you provide.
+    /// - Parameters:
+    ///   - lower: The lower bound for the scale's range.
+    ///   - higher: The upper bound for the scale's range.
+    /// - Returns: A copy of the scale with the range values you provide.
+    public func range(lower: OutputType, higher: OutputType) -> Self {
+        type(of: self).init(from: domainLower, to: domainHigher, transform: transformType, desiredTicks: desiredTicks, rangeLower: lower, rangeHigher: higher)
+    }
+
+    /// Returns a new scale with the range set to the values you provide.
+    /// - Parameters:
+    ///   - range: The range to apply as the scale's range.
+    /// - Returns: A copy of the scale with the range values you provide.
+    public func range(_ range: ClosedRange<OutputType>) -> Self {
+        type(of: self).init(from: domainLower, to: domainHigher, transform: transformType, desiredTicks: desiredTicks, rangeLower: range.lowerBound, rangeHigher: range.upperBound)
+    }
+
+    /// Returns a new scale with the transform set to the value you provide.
+    /// - Parameters:
+    ///   - transform: The transform constraint to apply when values fall outside the domain of the scale.
+    /// - Returns: A copy of the scale with the transform setting you provide.
+    public func transform(_ transform: DomainDataTransform) -> Self {
+        type(of: self).init(from: domainLower, to: domainHigher, transform: transform, desiredTicks: desiredTicks, rangeLower: rangeLower, rangeHigher: rangeHigher)
+    }
+
+    // MARK: - scale functions
+
+    /// Transforms the input value using a linear function to the resulting value into the range you provide.
+    ///
+    /// - Parameter domainValue: A value in the domain of the scale.
+    /// - Returns: A value mapped to the range you provide.
+    public func scale(_ domainValue: InputType) -> OutputType? {
+        guard let domainValue = transformAgainstDomain(domainValue), let rangeLower = rangeLower, let rangeHigher = rangeHigher else {
+            return nil
+        }
+        let normalizedInput = normalize(domainValue.toDouble(), lower: domainLower.toDouble(), higher: domainHigher.toDouble())
+        let result: Double = interpolate(normalizedInput, lower: rangeLower.toDouble(), higher: rangeHigher.toDouble())
+        return OutputType.fromDouble(result)
     }
 
     /// Transforms the input value using a linear function to the resulting value into the range you provide.
@@ -95,12 +142,25 @@ public struct LinearScale<InputType: ConvertibleWithDouble & NiceValue, OutputTy
     /// - Parameter higher: The upper bound of the range to map to.
     /// - Returns: A value mapped to the range you provide.
     public func scale(_ domainValue: InputType, from lower: OutputType, to higher: OutputType) -> OutputType? {
-        if let domainValue = transformAgainstDomain(domainValue) {
-            let normalizedInput = normalize(domainValue.toDouble(), lower: domainLower.toDouble(), higher: domainHigher.toDouble())
-            let result: Double = interpolate(normalizedInput, lower: lower.toDouble(), higher: higher.toDouble())
-            return OutputType.fromDouble(result)
+        let reconfigScale = range(lower: lower, higher: higher)
+        return reconfigScale.scale(domainValue)
+    }
+
+    /// Transforms a value within the range into the associated domain value.
+    /// - Parameters:
+    ///   - rangeValue: A value in the range of the scale.
+    ///   - lower: The lower bound to the range to map from.
+    ///   - higher: The upper bound to the range to map from.
+    /// - Returns: A value linearly mapped from the range back into the domain.
+    public func invert(_ rangeValue: OutputType) -> InputType? {
+        guard let rangeLower = rangeLower, let rangeHigher = rangeHigher else {
+            return nil
         }
-        return nil
+        // inverts the scale, taking a value in the output range and returning the relevant value from the input domain
+        let normalizedRangeValue = normalize(rangeValue.toDouble(), lower: rangeLower.toDouble(), higher: rangeHigher.toDouble())
+        let mappedToDomain = interpolate(normalizedRangeValue, lower: domainLower.toDouble(), higher: domainHigher.toDouble())
+        let castToInputType = InputType.fromDouble(mappedToDomain)
+        return transformAgainstDomain(castToInputType)
     }
 
     /// Transforms a value within the range into the associated domain value.
@@ -110,11 +170,8 @@ public struct LinearScale<InputType: ConvertibleWithDouble & NiceValue, OutputTy
     ///   - higher: The upper bound to the range to map from.
     /// - Returns: A value linearly mapped from the range back into the domain.
     public func invert(_ rangeValue: OutputType, from lower: OutputType, to higher: OutputType) -> InputType? {
-        // inverts the scale, taking a value in the output range and returning the relevant value from the input domain
-        let normalizedRangeValue = normalize(rangeValue.toDouble(), lower: lower.toDouble(), higher: higher.toDouble())
-        let mappedToDomain = interpolate(normalizedRangeValue, lower: domainLower.toDouble(), higher: domainHigher.toDouble())
-        let castToInputType = InputType.fromDouble(mappedToDomain)
-        return transformAgainstDomain(castToInputType)
+        let reconfigScale = range(lower: lower, higher: higher)
+        return reconfigScale.invert(rangeValue)
     }
 }
 
--- Sources/SwiftVizScale/LogScale.swift
+++ Sources/SwiftVizScale/LogScale.swift
@@ -15,16 +15,16 @@ public struct LogScale<InputType: ConvertibleWithDouble & NiceValue, OutputType:
     /// The distance or length between the upper and lower bounds of the input domain.
     public let domainExtent: InputType
 
+    /// The lower bound of the input domain.
+    public let rangeLower: OutputType?
+    /// The upper bound of the input domain.
+    public let rangeHigher: OutputType?
+
     /// A transformation value that indicates whether the output vales are constrained to the min and max of the output range.
     ///
     /// If `true`, values processed by the scale are constrained to the output range, and values processed backwards through the scale
     /// are constrained to the input domain.
     public var transformType: DomainDataTransform
-
-    /// A Boolean value that indicates the scale was configured without an explicit domain.
-    ///
-    /// Use ``withDomain(lower:higher:)`` to create a new scale with an explicit domain while keeping the same ``transformType``.
-    public var defaultDomain: Bool
 
     /// The number of ticks desired when creating the scale.
     ///
@@ -37,27 +37,19 @@ public struct LogScale<InputType: ConvertibleWithDouble & NiceValue, OutputType:
     ///   - higher: The upper bound for the scale's domain.
     ///   - transform: The transform constraint to apply when values fall outside the domain of the scale.
     ///   - desiredTicks: The desired number of ticks when visually representing the scale.
-    public init(from lower: InputType, to higher: InputType, transform: DomainDataTransform = .none, desiredTicks: Int = 10) {
-        precondition(lower < higher)
-        precondition(lower.toDouble() > 0.0)
+    public init(from lower: InputType = 1, to higher: InputType = 10, transform: DomainDataTransform = .none, desiredTicks: Int = 10, rangeLower: OutputType? = nil, rangeHigher: OutputType? = nil) {
+        precondition(lower < higher, "attempting to set an inverted or empty domain: \(lower) to \(higher)")
+        precondition(lower.toDouble() > 0.0, "attempting to set a log scale to start at 0.0")
         transformType = transform
         domainLower = lower
         domainHigher = higher
         domainExtent = higher - lower
         self.desiredTicks = desiredTicks
-        defaultDomain = false
-    }
-
-    /// Creates a new logarithmic scale with a default domain.
-    ///
-    /// The default domain for the scale is `1...10`.
-    /// Use this method to create a placeholder scale that you can refine to a fully configured scale with an updated domain using ``withDomain(lower:higher:)``.
-    /// - Parameters:
-    ///   - transform: The transform constraint to apply when values fall outside the domain of the scale.
-    ///   - desiredTicks: The desired number of ticks when visually representing the scale.
-    public init(exponent _: Double = 1, transform: DomainDataTransform = .none, desiredTicks: Int = 10) {
-        self.init(from: 1, to: 10, transform: transform, desiredTicks: desiredTicks)
-        defaultDomain = true
+        if let rangeLower = rangeLower, let rangeHigher = rangeHigher {
+            precondition(lower < higher, "attempting to set an inverted or empty range: \(rangeLower) to \(rangeHigher)")
+        }
+        self.rangeLower = rangeLower
+        self.rangeHigher = rangeHigher
     }
 
     /// Creates a new logarithmic scale for the upper and lower bounds of the domain range you provide.
@@ -65,17 +57,77 @@ public struct LogScale<InputType: ConvertibleWithDouble & NiceValue, OutputType:
     ///   - range: The range of the scale's domain.
     ///   - transform: The transform constraint to apply when values fall outside the domain of the scale.
     ///   - desiredTicks: The desired number of ticks when visually representing the scale.
-    public init(_ range: ClosedRange<InputType>, transform: DomainDataTransform = .none, desiredTicks: Int = 10) {
-        self.init(from: range.lowerBound, to: range.upperBound, transform: transform, desiredTicks: desiredTicks)
+    public init(_ range: ClosedRange<InputType>, transform: DomainDataTransform = .none, desiredTicks: Int = 10, rangeLower: OutputType? = nil, rangeHigher: OutputType? = nil) {
+        self.init(from: range.lowerBound, to: range.upperBound, transform: transform, desiredTicks: desiredTicks, rangeLower: rangeLower, rangeHigher: rangeHigher)
     }
+
+    // testing function
+    internal func fullyConfigured() -> Bool {
+        rangeLower != nil && rangeHigher != nil
+    }
+
+    // MARK: - modifier functions
 
     /// Returns a new scale with the domain set to the values you provide.
     /// - Parameters:
     ///   - lower: The lower bound for the scale's domain.
     ///   - higher: The upper bound for the scale's domain.
-    /// - Returns: A replica of the scale, preserving ``transformType`` while applying new domain values.
-    public func withDomain(lower: InputType, higher: InputType) -> LogScale<InputType, OutputType> {
-        type(of: self).init(from: lower, to: higher, transform: transformType, desiredTicks: desiredTicks)
+    /// - Returns: A copy of the scale with the domain values you provide.
+    public func domain(lower: InputType, higher: InputType) -> Self {
+        type(of: self).init(from: lower, to: higher, transform: transformType, desiredTicks: desiredTicks, rangeLower: rangeLower, rangeHigher: rangeHigher)
+    }
+
+    /// Returns a new scale with the domain set to the values you provide.
+    /// - Parameters:
+    ///   - range: The range to apply as the scale's domain
+    /// - Returns: A copy of the scale with the domain values you provide.
+    public func domain(_ range: ClosedRange<InputType>) -> Self {
+        type(of: self).init(from: range.lowerBound, to: range.upperBound, transform: transformType, desiredTicks: desiredTicks, rangeLower: rangeLower, rangeHigher: rangeHigher)
+    }
+
+    /// Returns a new scale with the range set to the values you provide.
+    /// - Parameters:
+    ///   - lower: The lower bound for the scale's range.
+    ///   - higher: The upper bound for the scale's range.
+    /// - Returns: A copy of the scale with the range values you provide.
+    public func range(lower: OutputType, higher: OutputType) -> Self {
+        type(of: self).init(from: domainLower, to: domainHigher, transform: transformType, desiredTicks: desiredTicks, rangeLower: lower, rangeHigher: higher)
+    }
+
+    /// Returns a new scale with the range set to the values you provide.
+    /// - Parameters:
+    ///   - range: The range to apply as the scale's range.
+    /// - Returns: A copy of the scale with the range values you provide.
+    public func range(_ range: ClosedRange<OutputType>) -> Self {
+        type(of: self).init(from: domainLower, to: domainHigher, transform: transformType, desiredTicks: desiredTicks, rangeLower: range.lowerBound, rangeHigher: range.upperBound)
+    }
+
+    /// Returns a new scale with the transform set to the value you provide.
+    /// - Parameters:
+    ///   - transform: The transform constraint to apply when values fall outside the domain of the scale.
+    /// - Returns: A copy of the scale with the transform setting you provide.
+    public func transform(_ transform: DomainDataTransform) -> Self {
+        type(of: self).init(from: domainLower, to: domainHigher, transform: transform, desiredTicks: desiredTicks, rangeLower: rangeLower, rangeHigher: rangeHigher)
+    }
+
+    // MARK: - scale functions
+
+    /// Transforms the input value using a linear function to the resulting value into the range you provide.
+    ///
+    /// - Parameter domainValue: A value in the domain of the scale.
+    /// - Parameter lower: The lower bound to the range to map to.
+    /// - Parameter higher: The upper bound of the range to map to.
+    /// - Returns: A value mapped to the range you provide.
+    public func scale(_ domainValue: InputType) -> OutputType? {
+        guard let domainValue = transformAgainstDomain(domainValue), let rangeLower = rangeLower, let rangeHigher = rangeHigher else {
+            return nil
+        }
+        let logDomainValue = log10(domainValue.toDouble())
+        let logDomainLower = log10(domainLower.toDouble())
+        let logDomainHigher = log10(domainHigher.toDouble())
+        let normalizedValueOnLogDomain = normalize(logDomainValue, lower: logDomainLower, higher: logDomainHigher)
+        let valueMappedToRange = interpolate(normalizedValueOnLogDomain, lower: rangeLower.toDouble(), higher: rangeHigher.toDouble())
+        return OutputType.fromDouble(valueMappedToRange)
     }
 
     /// Transforms the input value using a linear function to the resulting value into the range you provide.
@@ -85,15 +137,27 @@ public struct LogScale<InputType: ConvertibleWithDouble & NiceValue, OutputType:
     /// - Parameter higher: The upper bound of the range to map to.
     /// - Returns: A value mapped to the range you provide.
     public func scale(_ domainValue: InputType, from lower: OutputType, to higher: OutputType) -> OutputType? {
-        if let domainValue = transformAgainstDomain(domainValue) {
-            let logDomainValue = log10(domainValue.toDouble())
-            let logDomainLower = log10(domainLower.toDouble())
-            let logDomainHigher = log10(domainHigher.toDouble())
-            let normalizedValueOnLogDomain = normalize(logDomainValue, lower: logDomainLower, higher: logDomainHigher)
-            let valueMappedToRange = interpolate(normalizedValueOnLogDomain, lower: lower.toDouble(), higher: higher.toDouble())
-            return OutputType.fromDouble(valueMappedToRange)
+        let reconfiguredScale = range(lower: lower, higher: higher)
+        return reconfiguredScale.scale(domainValue)
+    }
+
+    /// Transforms a value within the range into the associated domain value.
+    /// - Parameters:
+    ///   - rangeValue: A value in the range of the scale.
+    ///   - lower: The lower bound to the range to map from.
+    ///   - higher: The upper bound to the range to map from.
+    /// - Returns: A value linearly mapped from the range back into the domain.
+    public func invert(_ rangeValue: OutputType) -> InputType? {
+        guard let rangeLower = rangeLower, let rangeHigher = rangeHigher else {
+            return nil
         }
-        return nil
+        let normalizedRangeValue = normalize(rangeValue.toDouble(), lower: rangeLower.toDouble(), higher: rangeHigher.toDouble())
+        let logDomainLower = log10(domainLower.toDouble())
+        let logDomainHigher = log10(domainHigher.toDouble())
+        let linearInterpolatedValue = interpolate(Double(normalizedRangeValue), lower: logDomainLower, higher: logDomainHigher)
+        let domainValue = pow(10, linearInterpolatedValue)
+        let downcastDomainValue = InputType.fromDouble(domainValue)
+        return transformAgainstDomain(downcastDomainValue)
     }
 
     /// Transforms a value within the range into the associated domain value.
@@ -103,12 +167,7 @@ public struct LogScale<InputType: ConvertibleWithDouble & NiceValue, OutputType:
     ///   - higher: The upper bound to the range to map from.
     /// - Returns: A value linearly mapped from the range back into the domain.
     public func invert(_ rangeValue: OutputType, from lower: OutputType, to higher: OutputType) -> InputType? {
-        let normalizedRangeValue = normalize(rangeValue.toDouble(), lower: lower.toDouble(), higher: higher.toDouble())
-        let logDomainLower = log10(domainLower.toDouble())
-        let logDomainHigher = log10(domainHigher.toDouble())
-        let linearInterpolatedValue = interpolate(Double(normalizedRangeValue), lower: logDomainLower, higher: logDomainHigher)
-        let domainValue = pow(10, linearInterpolatedValue)
-        let downcastDomainValue = InputType.fromDouble(domainValue)
-        return transformAgainstDomain(downcastDomainValue)
+        let reconfiguredScale = range(lower: lower, higher: higher)
+        return reconfiguredScale.invert(rangeValue)
     }
 }
