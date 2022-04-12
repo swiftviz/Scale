64f17c45991209d4a15941bed00ef7fdf7254c43
Joe Heck <heckj@mac.com>
2022-04-12 16:24:33 -0700

    enable domain of a single value for continuous scales

--- Tests/SwiftVizScaleTests/PowerScaleTests.swift
+++ Tests/SwiftVizScaleTests/PowerScaleTests.swift
@@ -125,4 +125,21 @@ class PowerScaleTests: XCTestCase {
         XCTAssertEqual(updated.domainLower, 1.0)
         XCTAssertEqual(updated.domainHigher, 15.0)
     }
+    
+    func testScaleDomainOfOneValue() {
+        let scale = PowerScale<Double, CGFloat>()
+        
+        let updated = scale.domain([5.0])
+        XCTAssertEqual(updated.domainLower, 5)
+        XCTAssertEqual(updated.domainHigher, 5)
+    }
+
+    func testScaleDomainOfOneValueNiced() {
+        let scale = PowerScale<Double, CGFloat>()
+        
+        let updated = scale.domain([5.0], nice: true)
+        XCTAssertEqual(updated.domainLower, 5)
+        XCTAssertEqual(updated.domainHigher, 5)
+    }
+
 }
