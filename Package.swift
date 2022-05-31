33447f84d95d3c8c9cebee9e62fba68ffe2eebb0
Joe Heck <heckj@mac.com>
2022-05-31 13:29:01 -0700

    stubbing in basic benchmarking code to evaluation performance for optimizations

--- Package.swift
+++ Package.swift
@@ -1,6 +1,7 @@
 // swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+import Foundation
 import PackageDescription
 
 let package = Package(
@@ -34,6 +35,24 @@ let package = Package(
         ),
     ]
 )
+
+if ProcessInfo.processInfo.environment["BENCHMARK"] != nil {
+    package.products.append(
+        .executable(name: "scale-benchmark", targets: ["scale-benchmark"])
+    )
+    package.dependencies.append(
+        .package(url: "https://github.com/google/swift-benchmark", from: "0.1.0")
+    )
+    package.targets.append(
+        .executableTarget(
+            name: "scale-benchmark",
+            dependencies: [
+                "SwiftVizScale",
+                .product(name: "Benchmark", package: "swift-benchmark"),
+            ]
+        )
+    )
+}
 
 #if swift(>=5.6)
     // Add the documentation compiler plugin if possible
