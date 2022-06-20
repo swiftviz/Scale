7ed4f030a0d77c690d051f0a6a1e325cd16ecc2d
Joe Heck <heckj@mac.com>
2022-06-20 10:24:54 -0700

    moving type of continuous scale enum into its own file

--- Sources/SwiftVizScale/AnyContinuousScale.swift
+++ Sources/SwiftVizScale/AnyContinuousScale.swift
@@ -7,44 +7,6 @@
 
 import Foundation
 
-/// The type of continuous scale.
-///
-/// Exponential scales (``PowerScale``) require an additional value for the exponent of the scale.
-public enum ContinuousScaleType: Equatable {
-    /// A linear continuous scale.
-    case linear
-    /// A logarithmic continuous scale.
-    case log
-    /// An exponential continuous scale.
-    case power(Double)
-    /// A linear continuous scale that squares the result.
-    case radial
-
-    var description: String {
-        switch self {
-        case .linear:
-            return "linear"
-        case .log:
-            return "log"
-        case .power:
-            return "power"
-        case .radial:
-            return "radial"
-        }
-    }
-    
-    var transform: (Double) -> Double {
-        switch self {
-        case .log:
-            return { Double.log($0) }
-        case .linear, .radial:
-            return { $0 }
-        case let .power(exp):
-            return { Double.pow($0, exp) }
-        }
-    }
-}
-
 // fatal error function (with line numbers to debug) that shows when you've accidentally
 // called a function on what should be an abstract base class.
 internal func _abstract(
---
+++ Sources/SwiftVizScale/ContinuousScaleType.swift
@@ -0,0 +1,42 @@
+//
+//  ContinuousScaleType.swift
+//  
+
+
+/// The type of continuous scale.
+///
+/// Exponential scales (``PowerScale``) require an additional value for the exponent of the scale.
+public enum ContinuousScaleType: Equatable {
+    /// A linear continuous scale.
+    case linear
+    /// A logarithmic continuous scale.
+    case log
+    /// An exponential continuous scale.
+    case power(Double)
+    /// A linear continuous scale that squares the result.
+    case radial
+
+    var description: String {
+        switch self {
+        case .linear:
+            return "linear"
+        case .log:
+            return "log"
+        case .power:
+            return "power"
+        case .radial:
+            return "radial"
+        }
+    }
+
+    var transform: (Double) -> Double {
+        switch self {
+        case .log:
+            return { Double.log($0) }
+        case .linear, .radial:
+            return { $0 }
+        case let .power(exp):
+            return { Double.pow($0, exp) }
+        }
+    }
+}
