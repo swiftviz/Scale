95f2c4bcfa27241fe4445073a32cd618cbf0255e
Joe Heck <heckj@mac.com>
2022-06-26 12:56:19 -0700

    verifying iterations and intervals in Tvalues for multi-step interpolator

--- Sources/SwiftVizScale/ArrayInterpolator.swift
+++ Sources/SwiftVizScale/ArrayInterpolator.swift
@@ -9,7 +9,7 @@ public struct ArrayInterpolator: Interpolator {
     // https://github.com/d3/d3-scale-chromatic/blob/main/README.md
     var colors: [CGColor]
 
-    static func interpolateIntoSteps(_ t: Double, _ count: Int) -> (Int, Int, Double) {
+    static func interpolateIntoSteps(_ t: Double, _ count: Int) -> (Int, Double) {
         precondition(t >= 0 && t <= 1)
         precondition(count > 1)
 
@@ -25,17 +25,17 @@ public struct ArrayInterpolator: Interpolator {
             lowerIndex = Int(floor(t / step))
         }
         // Calculate the unit-value for the two index steps
-        let lowerIndexTValue = 0 + step * Double(lowerIndex)
-        let upperIndexTValue = 0 + step * Double(lowerIndex + 1)
+        let lowerIndexTValue = step * Double(lowerIndex)
+        let upperIndexTValue = step * Double(lowerIndex + 1)
         // Determine a new unit-value that is the original 't' value interpolated between
         // the relevant steps.
         let tValueBetweenSteps = normalize(t, lower: lowerIndexTValue, higher: upperIndexTValue)
-        return (lowerIndex, lowerIndex + 1, tValueBetweenSteps)
+        return (lowerIndex, tValueBetweenSteps)
     }
 
     public func interpolate(_ t: Double) -> CGColor {
-        let (index1, index2, tBetweenIndicies) = Self.interpolateIntoSteps(t, colors.count)
-        return LCH.interpolate(colors[index1], colors[index2], t: tBetweenIndicies)
+        let (colorIndex, tBetweenIndicies) = Self.interpolateIntoSteps(t, colors.count - 1)
+        return LCH.interpolate(colors[colorIndex], colors[colorIndex + 1], t: tBetweenIndicies)
     }
 
     public init(_ from: CGColor, _ to: CGColor) {
--- Tests/SwiftVizScaleTests/ArrayInterpolatorTests.swift
+++ Tests/SwiftVizScaleTests/ArrayInterpolatorTests.swift
@@ -9,12 +9,35 @@ final class ArrayInterpolatorTests: XCTestCase {
     func testSweepInterpolateIntoSteps() throws {
         for count in 2 ... 10 {
             for tValue in 0 ... 10 {
-                let (index, index2, intermediateTValue) = ArrayInterpolator.interpolateIntoSteps(Double(tValue) / 10, count)
+                let (index, intermediateTValue) = ArrayInterpolator.interpolateIntoSteps(Double(tValue) / 10, count)
                 XCTAssertTrue(index >= 0)
                 XCTAssertTrue(index < count - 1)
-                XCTAssertEqual(index2, index + 1)
                 XCTAssertTrue((0.0 ... 1.0).contains(intermediateTValue))
             }
+        }
+    }
+
+    func testFiveStepInterpolationValues() throws {
+        // Five colors added means there'll be four breaks:
+        // 0, 0.25, 0.5, 0.75, and 1.0
+        let expectedTValueResults = [
+            0.0: (0, 0.0),
+            0.1: (0, 0.4),
+            0.2: (0, 0.8),
+            0.3: (1, 0.2),
+            0.4: (1, 0.6),
+            0.5: (2, 0.0),
+            0.6: (2, 0.4),
+            0.7: (2, 0.8),
+            0.8: (3, 0.2),
+            0.9: (3, 0.6),
+            1.0: (3, 1.0),
+        ]
+        for (tValue, resultSet) in expectedTValueResults {
+            let (index, intermediateTValue) = ArrayInterpolator.interpolateIntoSteps(tValue, 5)
+            print("index: \(index) t: \(intermediateTValue)")
+            XCTAssertEqual(index, resultSet.0)
+            XCTAssertEqual(intermediateTValue, resultSet.1, accuracy: 0.001)
         }
     }
 }
