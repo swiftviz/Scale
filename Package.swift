f280034a207027c43c72e33db3b746ca788505cf
Joe Heck <heckj@mac.com>
2022-06-26 13:28:40 -0700

    code cleanup

--- Package.swift
+++ Package.swift
@@ -13,7 +13,6 @@ let package = Package(
 //        .watchOS(.v3)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftVizScale",
             targets: ["SwiftVizScale"]
@@ -24,8 +23,6 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-numerics", from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftVizScale",
             dependencies: [.product(name: "Numerics", package: "swift-numerics")]
