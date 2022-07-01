6c0b9d8dd2db8db5c2e161d3f1b1294b8effb5c2
Joe Heck <heckj@mac.com>
2022-06-30 19:19:16 -0700

    fixing iOS build

--- Plugins/GenerateDocImages/main.swift
+++ Plugins/GenerateDocImages/main.swift
@@ -1,9 +1,9 @@
-//import Foundation
-//import PackagePlugin
+// import Foundation
+// import PackagePlugin
 //
-//@main
-//struct GenerateDocImages: CommandPlugin {
+// @main
+// struct GenerateDocImages: CommandPlugin {
 //    func performCommand(context _: PluginContext, arguments _: [String]) async throws {
 //        print("Hi")
 //    }
-//}
+// }
--- Sources/GenerateDocImages/GeneratedDocImagesCommand.swift
+++ Sources/GenerateDocImages/GeneratedDocImagesCommand.swift
@@ -4,34 +4,34 @@ import SystemPackage
 import VisualTests
 
 #if os(macOS)
-@main
-@available(macOS 12.0, *)
-struct GeneratedDocImagesCommand: ParsableCommand {
-    @Argument(help: "The phrase to repeat.")
-    var phrase: String
+    @main
+    @available(macOS 12.0, *)
+    struct GeneratedDocImagesCommand: ParsableCommand {
+        @Argument(help: "The phrase to repeat.")
+        var phrase: String
 
-    @MainActor
-    func run() throws {
-        print("Hi, \(phrase)")
+        @MainActor
+        func run() throws {
+            print("Hi, \(phrase)")
 
-        // damnit, I can't use this until macOS 13
+            // damnit, I can't use this until macOS 13
 //        if #available(macOS 13.0, *) {
 //            let renderer = ImageRenderer(content: ColorInterpolatorView(steps: 128, interpolator: .Viridis))
 //        } else {
-        let view = ColorInterpolatorView(steps: 128, interpolator: .Viridis)
-            .frame(width: 400, height: 40)
-        let image = view.snapshot()!.cgImage(forProposedRect: nil, context: nil, hints: nil)!
-        let newRepresentation = NSBitmapImageRep(cgImage: image)
+            let view = ColorInterpolatorView(steps: 128, interpolator: .Viridis)
+                .frame(width: 400, height: 40)
+            let image = view.snapshot()!.cgImage(forProposedRect: nil, context: nil, hints: nil)!
+            let newRepresentation = NSBitmapImageRep(cgImage: image)
 
-        let path: FilePath = "fiddlesticks.png"
-        let fd = try FileDescriptor.open(path, .writeOnly,
-                                         options: [.append, .create],
-                                         permissions: .ownerReadWrite)
-        try fd.closeAfter {
-            if let pngData = newRepresentation.representation(using: .png, properties: [:]) {
-                _ = try fd.writeAll(pngData)
+            let path: FilePath = "fiddlesticks.png"
+            let fd = try FileDescriptor.open(path, .writeOnly,
+                                             options: [.append, .create],
+                                             permissions: .ownerReadWrite)
+            try fd.closeAfter {
+                if let pngData = newRepresentation.representation(using: .png, properties: [:]) {
+                    _ = try fd.writeAll(pngData)
+                }
             }
         }
     }
-}
 #endif
