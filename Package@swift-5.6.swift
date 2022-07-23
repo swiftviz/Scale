75b12fb5a514c9df932078a43ce8be4e52acd58b
Joe Heck <heckj@mac.com>
2022-07-22 17:41:20 -0700

    adding watchOS support for 5.6+ swift package definition

--- Package@swift-5.6.swift
+++ Package@swift-5.6.swift
@@ -9,7 +9,7 @@ let package = Package(
         .iOS(.v13),
         .macOS(.v10_15),
         .tvOS(.v13),
-//        .watchOS(.v3)
+        .watchOS(.v6),
     ],
     products: [
         .library(
