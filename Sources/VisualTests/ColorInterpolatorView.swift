aa8396f24e69f4869c3b7c9fac98134123d3b535
Joe Heck <heckj@mac.com>
2022-07-27 12:54:40 -0700

    attempting to work around swift constraints with new some-type setup

--- Sources/VisualTests/ColorInterpolatorView.swift
+++ Sources/VisualTests/ColorInterpolatorView.swift
@@ -2,41 +2,43 @@
 //  ColorInterpolatorView.swift
 //
 
-import SwiftUI
-@testable import SwiftVizScale
+#if swift(>=5.6)
+    import SwiftUI
+    @testable import SwiftVizScale
 
-@available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
-public struct ColorInterpolatorView: View {
-    var steps: CGFloat
-    var interpolator: ColorInterpolator
+    @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
+    public struct ColorInterpolatorView: View {
+        var steps: CGFloat
+        var interpolator: ColorInterpolator
 
-    func color(_ stepValue: Int) -> CGColor {
-        let t = normalize(Double(stepValue),
-                          lower: 0.0,
-                          higher: steps - 1)
-        return interpolator.interpolate(t)
-    }
+        func color(_ stepValue: Int) -> CGColor {
+            let t = normalize(Double(stepValue),
+                              lower: 0.0,
+                              higher: steps - 1)
+            return interpolator.interpolate(t)
+        }
 
-    public var body: some View {
-        GeometryReader { proxy in
-            HStack(spacing: 0.0) {
-                ForEach(0 ... Int(steps - 1), id: \.self) { stepValue in
-                    Color(cgColor: color(stepValue))
-                        .frame(width: proxy.size.width / steps)
+        public var body: some View {
+            GeometryReader { proxy in
+                HStack(spacing: 0.0) {
+                    ForEach(0 ... Int(steps - 1), id: \.self) { stepValue in
+                        Color(cgColor: color(stepValue))
+                            .frame(width: proxy.size.width / steps)
+                    }
                 }
             }
         }
+
+        public init(steps: CGFloat, interpolator: some ColorInterpolator) {
+            self.steps = steps
+            self.interpolator = interpolator
+        }
     }
 
-    public init(steps: CGFloat, interpolator: some ColorInterpolator) {
-        self.steps = steps
-        self.interpolator = interpolator
+    @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
+    struct ColorInterpolatorView_Previews: PreviewProvider {
+        static var previews: some View {
+            ColorInterpolatorView(steps: 128, interpolator: ColorScheme.SequentialMultiHue.Cividis)
+        }
     }
-}
-
-@available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
-struct ColorInterpolatorView_Previews: PreviewProvider {
-    static var previews: some View {
-        ColorInterpolatorView(steps: 128, interpolator: ColorScheme.SequentialMultiHue.cividis)
-    }
-}
+#endif
