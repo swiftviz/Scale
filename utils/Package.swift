74860a6ec0414875ca6e95c39ed754d352039e99
Joe Heck <heckj@mac.com>
2022-07-27 11:56:11 -0700

    cleaning up bits left working around embedding in earlier package

--- utils/Package.swift
+++ utils/Package.swift
@@ -2,6 +2,11 @@
 
 import PackageDescription
 
+// This executable is in its own package because including a CLI executable package
+// into the Scale library caused Xcode to have a freak out, and fail to present
+// SwiftUI previews for targets within that package. By moving it out and into it's
+// own thing, Xcode's failing resolution mechanisms seem to be worked around.
+
 let package = Package(
     name: "GenerateDocImages",
     platforms: [
@@ -9,14 +14,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
-        .package(url: "https://github.com/swiftviz/scale", branch: "main"),
+        .package(url: "https://github.com/swiftviz/scale", branch: "externalCLI"),
         .package(url: "https://github.com/apple/swift-system", from: "0.0.3"),
     ],
     targets: [
         .executableTarget(
             name: "GenerateDocImages",
             dependencies: [
-                .product(name: "VisualTests", package: "scale"),
+                .product(name: "ScaleVisualTests", package: "scale"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "SystemPackage", package: "swift-system"),
             ]
--- utils/Sources/GenerateDocImages/GeneratedDocImagesCommand.swift
+++ utils/Sources/GenerateDocImages/GeneratedDocImagesCommand.swift
@@ -4,75 +4,62 @@ import SwiftVizScale
 import SystemPackage
 import VisualTests
 
-#if os(macOS) && swift(>=5.7)
-    @main
-    @available(macOS 12.0, *)
-    struct GeneratedDocImagesCommand: ParsableCommand {
-        @MainActor
-        func run() throws {
-            let schemes: [IndexedColorInterpolator: String] = [
-                ColorScheme.Diverging.BrBG: "BrBG",
-                ColorScheme.Diverging.PrGN: "PrGN",
-                ColorScheme.Diverging.PiYG: "PiYG",
-                ColorScheme.Diverging.PuOR: "PuOR",
-                ColorScheme.Diverging.RdBu: "RdBu",
-                ColorScheme.Diverging.RdGy: "RdGy",
-                ColorScheme.Diverging.RdYlBu: "RdYlBu",
-                ColorScheme.Diverging.RdYlGn: "RdYlGn",
-                ColorScheme.Diverging.Spectral: "Spectral",
-                ColorScheme.SequentialMultiHue.BuGn: "BuGn",
-                ColorScheme.SequentialMultiHue.BuPu: "BuPu",
-                ColorScheme.SequentialMultiHue.GnBu: "GnBu",
-                ColorScheme.SequentialMultiHue.OrRd: "OrRd",
-                ColorScheme.SequentialMultiHue.PuBu: "PuBu",
-                ColorScheme.SequentialMultiHue.PuBuGn: "PuBuGn",
-                ColorScheme.SequentialMultiHue.PuRd: "PuRd",
-                ColorScheme.SequentialMultiHue.RdPu: "RdPu",
-                ColorScheme.SequentialMultiHue.YlGn: "YlGn",
-                ColorScheme.SequentialMultiHue.YlGnBu: "YlGnBu",
-                ColorScheme.SequentialMultiHue.YlOrBr: "YlOrBr",
-                ColorScheme.SequentialMultiHue.YlOrRd: "YlOrRd",
-                ColorScheme.SequentialMultiHue.Viridis: "Viridis",
-                ColorScheme.SequentialMultiHue.Magma: "Magma",
-                ColorScheme.SequentialMultiHue.Inferno: "Inferno",
-                ColorScheme.SequentialMultiHue.Plasma: "Plasma",
-                ColorScheme.SequentialSingleHue.Oranges: "Oranges",
-                ColorScheme.SequentialSingleHue.Purples: "Purples",
-                ColorScheme.SequentialSingleHue.Grays: "Grays",
-                ColorScheme.SequentialSingleHue.Blues: "Blues",
-                ColorScheme.SequentialSingleHue.Greens: "Greens",
-                ColorScheme.SequentialSingleHue.Reds: "Reds",
-            ]
+@main
+@available(macOS 12.0, *)
+struct GeneratedDocImagesCommand: ParsableCommand {
+    @MainActor
+    func run() throws {
+        let schemes: [IndexedColorInterpolator: String] = [
+            ColorScheme.Diverging.BrBG: "BrBG",
+            ColorScheme.Diverging.PrGN: "PrGN",
+            ColorScheme.Diverging.PiYG: "PiYG",
+            ColorScheme.Diverging.PuOR: "PuOR",
+            ColorScheme.Diverging.RdBu: "RdBu",
+            ColorScheme.Diverging.RdGy: "RdGy",
+            ColorScheme.Diverging.RdYlBu: "RdYlBu",
+            ColorScheme.Diverging.RdYlGn: "RdYlGn",
+            ColorScheme.Diverging.Spectral: "Spectral",
+            ColorScheme.SequentialMultiHue.BuGn: "BuGn",
+            ColorScheme.SequentialMultiHue.BuPu: "BuPu",
+            ColorScheme.SequentialMultiHue.GnBu: "GnBu",
+            ColorScheme.SequentialMultiHue.OrRd: "OrRd",
+            ColorScheme.SequentialMultiHue.PuBu: "PuBu",
+            ColorScheme.SequentialMultiHue.PuBuGn: "PuBuGn",
+            ColorScheme.SequentialMultiHue.PuRd: "PuRd",
+            ColorScheme.SequentialMultiHue.RdPu: "RdPu",
+            ColorScheme.SequentialMultiHue.YlGn: "YlGn",
+            ColorScheme.SequentialMultiHue.YlGnBu: "YlGnBu",
+            ColorScheme.SequentialMultiHue.YlOrBr: "YlOrBr",
+            ColorScheme.SequentialMultiHue.YlOrRd: "YlOrRd",
+            ColorScheme.SequentialMultiHue.Viridis: "Viridis",
+            ColorScheme.SequentialMultiHue.Magma: "Magma",
+            ColorScheme.SequentialMultiHue.Inferno: "Inferno",
+            ColorScheme.SequentialMultiHue.Plasma: "Plasma",
+            ColorScheme.SequentialSingleHue.Oranges: "Oranges",
+            ColorScheme.SequentialSingleHue.Purples: "Purples",
+            ColorScheme.SequentialSingleHue.Grays: "Grays",
+            ColorScheme.SequentialSingleHue.Blues: "Blues",
+            ColorScheme.SequentialSingleHue.Greens: "Greens",
+            ColorScheme.SequentialSingleHue.Reds: "Reds",
+        ]
 
-            for (interpolator, name) in schemes {
-                print("Creating color swatch for \(name)")
+        for (interpolator, name) in schemes {
+            print("Creating color swatch for \(name)")
 
-                let view = ColorInterpolatorView(steps: 128, interpolator: interpolator)
-                    .frame(width: 400, height: 40)
-                let image = view.snapshot()!.cgImage(forProposedRect: nil, context: nil, hints: nil)!
-                let newRepresentation = NSBitmapImageRep(cgImage: image)
+            let view = ColorInterpolatorView(steps: 128, interpolator: interpolator)
+                .frame(width: 400, height: 40)
+            let image = view.snapshot()!.cgImage(forProposedRect: nil, context: nil, hints: nil)!
+            let newRepresentation = NSBitmapImageRep(cgImage: image)
 
-                let path = FilePath("\(name).png")
-                let fd = try FileDescriptor.open(path, .writeOnly,
-                                                 options: [.append, .create],
-                                                 permissions: .ownerReadWrite)
-                try fd.closeAfter {
-                    if let pngData = newRepresentation.representation(using: .png, properties: [:]) {
-                        _ = try fd.writeAll(pngData)
-                    }
+            let path = FilePath("\(name).png")
+            let fd = try FileDescriptor.open(path, .writeOnly,
+                                             options: [.append, .create],
+                                             permissions: .ownerReadWrite)
+            try fd.closeAfter {
+                if let pngData = newRepresentation.representation(using: .png, properties: [:]) {
+                    _ = try fd.writeAll(pngData)
                 }
             }
-            // damnit, I can't use this until macOS 13
-//        if #available(macOS 13.0, *) {
-//            let renderer = ImageRenderer(content: ColorInterpolatorView(steps: 128, interpolator: .Viridis))
-//        } else {
         }
     }
-#else
-    @main
-    struct GeneratedDocImagesCommand: ParsableCommand {
-        func run() throws {
-            print("NOT IMPLEMENTED for Swift < 5.7")
-        }
-    }
-#endif
+}
