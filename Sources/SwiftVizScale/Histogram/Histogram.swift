f8009f060db1e7af6c478632f0fad3bacd39f769
Joe Heck <heckj@mac.com>
2022-07-31 16:08:13 -0700

    adding bounds to histogram, isEmpty property, and updating documentation

--- Sources/SwiftVizScale/Histogram/Histogram.swift
+++ Sources/SwiftVizScale/Histogram/Histogram.swift
@@ -19,20 +19,33 @@ import OrderedCollections
 // Collection.bin -> Histogram(data: _)
 // Collection.bin(stride: _) -> Histogram(data: self, stride: stride)
 
+/// A histogram representing a distribution of numeric data.
 public struct Histogram<Value: Numeric & Hashable & Comparable> {
     typealias InternalDictType = OrderedDictionary<HistogramBinRange<Value>, Int>
     private var _storage: InternalDictType
+    
+    /// The lowest value of the histogram's range.
+    public var lowerBound: Value
+    /// The highest value of the histogram's range.
+    public var upperBound: Value
+    /// A Boolean value that indicates of the histogram is empty.
+    public var isEmpty: Bool
 
     // MARK: - Automatic (Scott's Rule) Initializers
 
     /// Automatically determine the bins from data.
-    /// - Parameter data: <#data description#>
+    /// - Parameter data: The data to populate the histogram.
     public init(data: [Value]) where Value: BinaryInteger {
         _storage = [:]
         guard let stdDev = data.stdDev, let smallestValue = data.min(), let largestValue = data.max() else {
+            lowerBound = 0
+            upperBound = 0
+            isEmpty = true
             return
         }
-
+        isEmpty = false
+        lowerBound = smallestValue
+        upperBound = largestValue
         // As a default, use Scott's normal reference rule: 3.49 * stdDev / pow(n, 1/3) to select a uniform
         // bin size for the histogram, with the assumption that the distribution is roughly normal.
         let binSize = round(3.49 * stdDev / pow(Double(data.count), 1.0 / 3.0))
@@ -56,12 +69,19 @@ public struct Histogram<Value: Numeric & Hashable & Comparable> {
     }
 
     /// Automatically determine the bins from data.
-    /// - Parameter data: <#data description#>
+    /// - Parameter data: The data to populate the histogram.
     public init(data: [Value]) where Value: BinaryFloatingPoint {
         _storage = [:]
         guard let stdDev = data.stdDev, let smallestValue = data.min(), let largestValue = data.max() else {
+            lowerBound = 0
+            upperBound = 0
+            isEmpty = true
             return
         }
+        isEmpty = false
+        lowerBound = smallestValue
+        upperBound = largestValue
+
         // As a default, use Scott's normal reference rule: 3.49 * stdDev / pow(n, 1/3) to determine a uniform
         // bin size for the histogram.
         let binSize = 3.49 * stdDev / pow(Double(data.count), 1.0 / 3.0)
@@ -88,13 +108,25 @@ public struct Histogram<Value: Numeric & Hashable & Comparable> {
 
     /// Automatically determine the bins from data, constrained by a minimum bin size
     /// - Parameters:
-    ///   - data: <#data description#>
-    ///   - desiredCount: <#desiredCount description#>
-    ///   - minimumStride: <#minimumStride description#>
-    public init(data: [Value], minimumStride: Value, desiredCount: Int? = nil) where Value: BinaryFloatingPoint {
+    ///   - data: The data to populate the histogram.
+    ///   - minimumStride: The minimum bin size to group the counts.
+    ///   - bounds: The minimum and maximum value that the histogram supports.
+    ///   - desiredCount: An optional value of the number of bins to create. If `nil`, the number of bins is determined by the minimum stride you provide.
+    public init(data: [Value], minimumStride: Value, bounds: ClosedRange<Value>? = nil, desiredCount: Int? = nil) where Value: BinaryFloatingPoint {
         _storage = [:]
         guard let smallestValue = data.min(), let largestValue = data.max() else {
+            lowerBound = 0
+            upperBound = 0
+            isEmpty = true
             return
+        }
+        isEmpty = true
+        if let setBounds = bounds {
+            lowerBound = setBounds.lowerBound
+            upperBound = setBounds.upperBound
+        } else {
+            lowerBound = smallestValue
+            upperBound = largestValue
         }
         // start by breaking down the data into discrete chunks - building the keys based on information
         // density, explicit values, or fixed widths,
@@ -104,18 +136,18 @@ public struct Histogram<Value: Numeric & Hashable & Comparable> {
         if let desiredCount = desiredCount {
             // if a desired number of bins was provided, and a stride value evenly dividing the
             // data's range into those values is larger than the minimum, use that larger stride.
-            let maybeStride = (largestValue - smallestValue) / Value(desiredCount)
+            let maybeStride = (upperBound - lowerBound) / Value(desiredCount)
             if maybeStride >= minimumStride {
                 stride = maybeStride
             }
         }
 
         // Create and initialize the keys for the bins with a count of 0
-        var currentStep = smallestValue
-        while currentStep < largestValue {
+        var currentStep = lowerBound
+        while currentStep < upperBound {
             let bin: HistogramBinRange<Value>
-            if currentStep + stride > largestValue {
-                bin = HistogramBinRange(lowerBound: currentStep, upperBound: largestValue, _final: true)
+            if currentStep + stride > upperBound {
+                bin = HistogramBinRange(lowerBound: currentStep, upperBound: upperBound, _final: true)
             } else {
                 bin = HistogramBinRange(lowerBound: currentStep, upperBound: currentStep + stride)
             }
@@ -129,13 +161,25 @@ public struct Histogram<Value: Numeric & Hashable & Comparable> {
 
     /// Automatically determine the bins from data, constrained by a minimum bin size
     /// - Parameters:
-    ///   - data: <#data description#>
-    ///   - desiredCount: <#desiredCount description#>
-    ///   - minimumStride: <#minimumStride description#>
-    public init(data: [Value], minimumStride: Value, desiredCount: Int? = nil) where Value: BinaryInteger {
+    ///   - data: The data to populate the histogram.
+    ///   - minimumStride: The minimum bin size to group the counts.
+    ///   - bounds: The minimum and maximum value that the histogram supports.
+    ///   - desiredCount: An optional value of the number of bins to create. If `nil`, the number of bins is determined by the minimum stride you provide.
+    public init(data: [Value], minimumStride: Value, bounds: ClosedRange<Value>? = nil, desiredCount: Int? = nil) where Value: BinaryInteger {
         _storage = [:]
         guard let smallestValue = data.min(), let largestValue = data.max() else {
+            lowerBound = 0
+            upperBound = 0
+            isEmpty = true
             return
+        }
+        isEmpty = false
+        if let setBounds = bounds {
+            lowerBound = setBounds.lowerBound
+            upperBound = setBounds.upperBound
+        } else {
+            lowerBound = smallestValue
+            upperBound = largestValue
         }
         // start by breaking down the data into discrete chunks - building the keys based on information
         // density, explicit values, or fixed widths,
@@ -145,18 +189,18 @@ public struct Histogram<Value: Numeric & Hashable & Comparable> {
         if let desiredCount = desiredCount {
             // if a desired number of bins was provided, and a stride value evenly dividing the
             // data's range into those values is larger than the minimum, use that larger stride.
-            let maybeStride = round((Double(largestValue) - Double(smallestValue)) / Double(desiredCount))
+            let maybeStride = round((Double(upperBound) - Double(lowerBound)) / Double(desiredCount))
             if maybeStride >= Double(minimumStride) {
                 stride = Value(maybeStride)
             }
         }
 
         // Create and initialize the keys for the bins with a count of 0
-        var currentStep = smallestValue
-        while currentStep < largestValue {
+        var currentStep = lowerBound
+        while currentStep < upperBound {
             let bin: HistogramBinRange<Value>
-            if currentStep + stride > largestValue {
-                bin = HistogramBinRange(lowerBound: currentStep, upperBound: largestValue, _final: true)
+            if currentStep + stride > upperBound {
+                bin = HistogramBinRange(lowerBound: currentStep, upperBound: upperBound, _final: true)
             } else {
                 bin = HistogramBinRange(lowerBound: currentStep, upperBound: currentStep + stride)
             }
@@ -170,14 +214,26 @@ public struct Histogram<Value: Numeric & Hashable & Comparable> {
 
     // MARK: - Explicit Thresholds, Non-uniform bin-size Initializers
 
-    /// Explicitly build a histogram with N-1 threshold bins, with the explicit thresholds you provide.
-    /// - Parameter data: <#data description#>
+    /// Explicitly build a histogram with the explicit thresholds you provide.
+    ///
+    /// Use this initializer to create a histogram with non-uniform bin sizes.
+    /// The number of bins created is one less than the number of thresholds you provide.
+    ///
+    /// - Parameters:
+    ///   - data: The data to populate the histogram.
+    ///   - thresholds: The values to use as the thresholds for the bins.
     public init(data: [Value], thresholds: [Value]) {
         _storage = [:]
         let sortedThresholds = thresholds.sorted()
-        guard let lastThreshold = sortedThresholds.last, data.count > 1, sortedThresholds.count > 1 else {
+        guard let firstThreshold = sortedThresholds.first, let lastThreshold = sortedThresholds.last, data.count > 1, sortedThresholds.count > 1 else {
+            lowerBound = 0
+            upperBound = 0
+            isEmpty = true
             return
         }
+        isEmpty = false
+        lowerBound = firstThreshold
+        upperBound = lastThreshold
 
         // Create and initialize the keys for the bins with a count of 0
         for (lower, higher) in sortedThresholds.pairs() {
