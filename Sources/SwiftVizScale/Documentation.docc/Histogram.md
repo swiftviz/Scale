7be7243bb9868ee2e8af481e5368822d16a47877
Joe Heck <heckj@mac.com>
2022-08-08 13:34:02 -0700

    cleaning up docs and reporting

--- Sources/SwiftVizScale/Documentation.docc/Histogram.md
+++ Sources/SwiftVizScale/Documentation.docc/Histogram.md
@@ -6,8 +6,8 @@
 
 - ``SwiftVizScale/Histogram/init(data:)-4w0u8``
 - ``SwiftVizScale/Histogram/init(data:)-6k9pg``
-- ``SwiftVizScale/Histogram/init(data:minimumStride:desiredCount:)-77r56``
-- ``SwiftVizScale/Histogram/init(data:minimumStride:desiredCount:)-9jf6n``
+- ``SwiftVizScale/Histogram/init(data:minimumStride:bounds:desiredCount:)-1kxi5``
+- ``SwiftVizScale/Histogram/init(data:minimumStride:bounds:desiredCount:)-2c242``
 - ``SwiftVizScale/Histogram/init(data:thresholds:)``
 
 ### Describing Histogram
--- docreport.bash
+++ docreport.bash
@@ -3,18 +3,20 @@
 set -e  # exit on a non-zero return code from a command
 set -x  # print a trace of commands as they execute
 
-rm -rf .build
-mkdir -p .build/symbol-graphs
+rm -rf .build .scale-graphs .scaleview-graphs
+mkdir -p .scale-graphs
 
 $(xcrun --find swift) build --target SwiftVizScale \
     -Xswiftc -emit-symbol-graph \
-    -Xswiftc -emit-symbol-graph-dir -Xswiftc .build/symbol-graphs
+    -Xswiftc -emit-symbol-graph-dir -Xswiftc .scale-graphs
+    
+rm -f ./scale-graphs/Collections*.json ./scale-graphs/DequeModule*.json ./scale-graphs/OrderedCollections*.json
 
 $(xcrun --find docc) convert Sources/SwiftVizScale/Documentation.docc \
     --analyze \
     --fallback-display-name SwiftVizScale \
     --fallback-bundle-identifier com.github.swiftviz.SwiftVizScale \
     --fallback-bundle-version 0.1.9 \
-    --additional-symbol-graph-dir .build/symbol-graphs \
+    --additional-symbol-graph-dir .scale-graphs \
     --experimental-documentation-coverage \
     --level brief
