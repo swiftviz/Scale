7a932eaa139200885246366b8cb164f1d82d4d4b
Joe Heck <heckj@mac.com>
2023-02-28 15:23:51 -0800

    updating preview.bash

--- preview.bash
+++ preview.bash
@@ -1,13 +1,6 @@
- $(xcrun --find swift) package \
-     --disable-sandbox \
-     --allow-writing-to-directory ./docs \
-     preview-documentation \
-     --fallback-bundle-identifier com.github.swiftviz.SwiftVizScale \
-     --target SwiftVizScale \
-     --output-path ./docs \
-     --emit-digest \
-     --disable-indexing \
-     --transform-for-static-hosting \
-     --hosting-base-path 'Scale'
+#!/bin/bash
 
+set -x
 
+$(xcrun --find swift) package --disable-sandbox \
+     preview-documentation --target SwiftVizScale
